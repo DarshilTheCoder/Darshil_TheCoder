f36c9c9f085ed24516ac47e836b0cef7e99e1271
DarshilTheCoder <102150953+DarshilTheCoder@users.noreply.github.com>
2022-04-11 15:39:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName(".navbar").style.display = "none"
+document.getElementById("#bar").addEventListener("click",()=>{
+    document.getElementById("#ul").style.display = "block"
+})
+.document.getElementById("#cross").addEventListener("click",()=>{
+    let a = document.getElementsByClassName(".navbar")
+    a.style.display = "none"
+})
